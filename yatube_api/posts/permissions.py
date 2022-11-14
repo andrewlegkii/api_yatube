e346882e2ef2f80c0038d2d2f987ab1cf6f67255
Андрей Легкий <legkiy.andrey@bk.ru>
2022-11-14 21:57:22 +0300

    1

---
+++ yatube_api/posts/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Для владельца.
+    """
+    def has_object_permission(self, request, view, obj):
+        if (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user):
+            return True
---
+++ yatube_api/posts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+        extra_kwargs = {'author': {'required': True}}
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
