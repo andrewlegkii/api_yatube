6f8e3f3630505ce40cd140fda75e8f5bd0544d28
Андрей Легкий <legkiy.andrey@bk.ru>
2022-11-15 11:31:09 +0300

    2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -13,6 +13,7 @@ class PostSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
+
     class Meta:
         model = Post
         fields = '__all__'
